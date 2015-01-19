56e0830aa37741238010a8101b0e26a7356bb15e
finico <finico.a@gmail.com>
2015-01-20 00:32:51 +0300

    homework 01

---
+++ homework/homework-01.swift
@@ -0,0 +1,9 @@
+let firstName = "Alexey"
+let lastName = "Finico"
+let age = 27
+let height = 187
+let weight = 80
+let prorgammingLanguage = "Swift"
+let teacher = "Alex Skutarenko"
+
+println("Hi! My name is \(firstName) \(lastName). I'm \(age) years old. My height is \(height) cm, weight is \(weight) kg. I'm currently studying the programming language \(prorgammingLanguage) and want to say thanks for it to \(teacher).")
