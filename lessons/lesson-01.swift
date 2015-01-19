090cf2cb91d99e9aea9b547f5d22832f478aa6fb
finico <finico.a@gmail.com>
2015-01-20 00:32:23 +0300

    lesson 01

---
+++ lessons/lesson-01.swift
@@ -0,0 +1,23 @@
+let redColor = "red"
+let greenColor = "green"
+let blueColor = "blue"
+
+var color : String // Type definition
+
+color = redColor
+println(color)
+
+color = greenColor
+println(color)
+
+color = "someColor"
+println(color)
+
+print(redColor)
+print(greenColor)
+print(blueColor)
+
+println()
+
+println("Red color value = \(redColor)")
+println("Green color value = \(greenColor)\nBlue color value = \(blueColor)")
