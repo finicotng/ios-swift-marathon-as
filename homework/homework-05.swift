65c29e092b16f6566959a02acf6414df16094759
finico <finico.a@gmail.com>
2015-01-29 21:26:04 +0300

    homework 05 4/4

--- homework/homework-05.swift
+++ homework/homework-05.swift
@@ -22,11 +22,10 @@ result = (result + dayOfBirth) * secondsPerDay
 
 println("From the beginning of the year before my birthday passes \(result) seconds")
 
+
 // part 2
 
-let absoluteMonthOfBirth = monthOfBirth - 1
-
-let quarterOfBirth = absoluteMonthOfBirth / (12 / 4) + 1
+let quarterOfBirth = (monthOfBirth - 1) / (12 / 4) + 1
 
 let numeralEnd = quarterOfBirth == 1 ?
     "st" : quarterOfBirth == 2 ?
@@ -34,3 +33,35 @@ let numeralEnd = quarterOfBirth == 1 ?
             "rd" : "th"
 
 println("I was born in the \(quarterOfBirth)\(numeralEnd) quarter of the year")
+
+
+// part 3
+var (n1, n2, n3, n4, n5) = (1, 2, 3, 4, 5)
+
+let result1 = ++n1 + ++n2 * n3-- / --n4 - n5
+let result2 = n3++ * n1++ / ++n4 / --n5 / --n2
+let result3 = n5++ - n4-- + ++n3 - n2-- - --n1
+// the results should always be 0
+
+
+// part 4
+let x = 4
+let y = 6
+
+let color = (x % 2 == 0) && (y % 2 != 0) ?
+    "black" : (x % 2 != 0) && (y % 2 == 0) ?
+        "black" : "white"
+
+println(color)
+
+
+// bonus
+for x in 1...8 {
+    for y in 1...8 {
+        let color = (x % 2 == 0) && (y % 2 != 0) ?
+            "#" : (x % 2 != 0) && (y % 2 == 0) ?
+                "#" : " "
+        print(color)
+    }
+    println()
+}
